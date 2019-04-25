d802487f55cbe21cec69f10b18e8f9098be2fa1d
rsthakre <rsthakre@gmail.com>
2019-04-25 14:41:15 -0400

    Initial transfer of Apple Picker

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+
+        }
+
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TextMesh scoreGT = this.GetComponent<TextMesh>();
+        scoreGT.text = "High Score:" + score;
+        if(score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+
+        }
+    }
+}
